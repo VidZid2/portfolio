34de3a9915d0d06d296615072cad308e30505c5c
VidZid2 <urbanninvidz2@gmail.com>
2026-08-22 10:35:55 +0800

    feat(blueprint): expand margin lines in timescale section to frame timeline without cutting through cards

--- src/components/BlueprintGrid.tsx
+++ src/components/BlueprintGrid.tsx
@@ -10,6 +10,7 @@ interface BlueprintGridProps {
   children?: React.ReactNode;
   showRightNavbar?: boolean;
   className?: string;
+  expandContentMargins?: boolean;
 }
 
 const DOT_MASK_VERTICAL = {
@@ -28,21 +29,62 @@ export function BlueprintGrid({
   children,
   showRightNavbar = true,
   className = "",
+  expandContentMargins = false,
 }: BlueprintGridProps) {
   return (
     <div className={`min-h-screen w-full bg-white dark:bg-black relative overflow-x-hidden transition-colors duration-300 ${className}`}>
       {/* Right Side Blueprint Navigation */}
       {showRightNavbar && <RightNavbar />}
 
-      {/* Vertical Lines - Ultra-fine Micro Dots (Standard 24.5% margins on desktop) */}
-      <div
-        className="absolute top-0 bottom-0 left-3 sm:left-4 md:left-[24.5%] w-0 border-r border-black/30 dark:border-white/[0.15] pointer-events-none"
-        style={DOT_MASK_VERTICAL}
-      />
-      <div
-        className="absolute top-0 bottom-0 right-3 sm:right-4 md:right-[24.5%] w-0 border-r border-black/30 dark:border-white/[0.15] pointer-events-none"
-        style={DOT_MASK_VERTICAL}
-      />
+      {expandContentMargins ? (
+        <>
+          {/* Top Section Vertical Lines (0 to Header Bottom) - Standard 24.5% Header Margins */}
+          <div
+            className="absolute top-0 h-[calc(22vh+112px)] left-3 sm:left-4 md:left-[24.5%] w-0 border-r border-black/30 dark:border-white/[0.15] pointer-events-none"
+            style={DOT_MASK_VERTICAL}
+          />
+          <div
+            className="absolute top-0 h-[calc(22vh+112px)] right-3 sm:right-4 md:right-[24.5%] w-0 border-r border-black/30 dark:border-white/[0.15] pointer-events-none"
+            style={DOT_MASK_VERTICAL}
+          />
+
+          {/* Lower Content Section Vertical Lines (Header Bottom to Page Bottom) - Expanded to Fit Timescale */}
+          <div
+            className="absolute top-[calc(22vh+112px)] bottom-0 left-3 sm:left-4 md:left-6 lg:left-8 w-0 border-r border-black/30 dark:border-white/[0.15] pointer-events-none"
+            style={DOT_MASK_VERTICAL}
+          />
+          <div
+            className="absolute top-[calc(22vh+112px)] bottom-0 right-3 sm:right-4 md:right-6 lg:right-8 w-0 border-r border-black/30 dark:border-white/[0.15] pointer-events-none"
+            style={DOT_MASK_VERTICAL}
+          />
+
+          {/* Ultra-Tiny Solid Nodes */}
+          <div className="absolute top-[22vh] left-3 sm:left-4 md:left-[24.5%] w-[2px] h-[2px] bg-black/50 dark:bg-white/[0.25] -translate-x-1/2 -translate-y-1/2 pointer-events-none z-10" />
+          <div className="absolute top-[22vh] right-3 sm:right-4 md:right-[24.5%] w-[2px] h-[2px] bg-black/50 dark:bg-white/[0.25] translate-x-1/2 -translate-y-1/2 pointer-events-none z-10" />
+          <div className="absolute top-[calc(22vh+112px)] left-3 sm:left-4 md:left-[24.5%] w-[2px] h-[2px] bg-black/50 dark:bg-white/[0.25] -translate-x-1/2 -translate-y-1/2 pointer-events-none z-10" />
+          <div className="absolute top-[calc(22vh+112px)] right-3 sm:right-4 md:right-[24.5%] w-[2px] h-[2px] bg-black/50 dark:bg-white/[0.25] translate-x-1/2 -translate-y-1/2 pointer-events-none z-10" />
+          <div className="absolute top-[calc(22vh+112px)] left-3 sm:left-4 md:left-6 lg:left-8 w-[2px] h-[2px] bg-black/50 dark:bg-white/[0.25] -translate-x-1/2 -translate-y-1/2 pointer-events-none z-10" />
+          <div className="absolute top-[calc(22vh+112px)] right-3 sm:right-4 md:right-6 lg:right-8 w-[2px] h-[2px] bg-black/50 dark:bg-white/[0.25] translate-x-1/2 -translate-y-1/2 pointer-events-none z-10" />
+        </>
+      ) : (
+        <>
+          {/* Vertical Lines - Ultra-fine Micro Dots (Standard 24.5% margins on desktop) */}
+          <div
+            className="absolute top-0 bottom-0 left-3 sm:left-4 md:left-[24.5%] w-0 border-r border-black/30 dark:border-white/[0.15] pointer-events-none"
+            style={DOT_MASK_VERTICAL}
+          />
+          <div
+            className="absolute top-0 bottom-0 right-3 sm:right-4 md:right-[24.5%] w-0 border-r border-black/30 dark:border-white/[0.15] pointer-events-none"
+            style={DOT_MASK_VERTICAL}
+          />
+
+          {/* Ultra-Tiny Solid Nodes */}
+          <div className="absolute top-[22vh] left-3 sm:left-4 md:left-[24.5%] w-[2px] h-[2px] bg-black/50 dark:bg-white/[0.25] -translate-x-1/2 -translate-y-1/2 pointer-events-none z-10" />
+          <div className="absolute top-[22vh] right-3 sm:right-4 md:right-[24.5%] w-[2px] h-[2px] bg-black/50 dark:bg-white/[0.25] translate-x-1/2 -translate-y-1/2 pointer-events-none z-10" />
+          <div className="absolute top-[calc(22vh+112px)] left-3 sm:left-4 md:left-[24.5%] w-[2px] h-[2px] bg-black/50 dark:bg-white/[0.25] -translate-x-1/2 -translate-y-1/2 pointer-events-none z-10" />
+          <div className="absolute top-[calc(22vh+112px)] right-3 sm:right-4 md:right-[24.5%] w-[2px] h-[2px] bg-black/50 dark:bg-white/[0.25] translate-x-1/2 -translate-y-1/2 pointer-events-none z-10" />
+        </>
+      )}
 
       {/* Horizontal Lines - Ultra-fine Micro Dots */}
       <div
@@ -53,12 +95,6 @@ export function BlueprintGrid({
         className="absolute left-0 right-0 top-[calc(22vh+112px)] h-0 border-b border-black/30 dark:border-white/[0.15] pointer-events-none"
         style={DOT_MASK_HORIZONTAL}
       />
-
-      {/* Ultra-Tiny Solid Nodes */}
-      <div className="absolute top-[22vh] left-3 sm:left-4 md:left-[24.5%] w-[2px] h-[2px] bg-black/50 dark:bg-white/[0.25] -translate-x-1/2 -translate-y-1/2 pointer-events-none z-10" />
-      <div className="absolute top-[22vh] right-3 sm:right-4 md:right-[24.5%] w-[2px] h-[2px] bg-black/50 dark:bg-white/[0.25] translate-x-1/2 -translate-y-1/2 pointer-events-none z-10" />
-      <div className="absolute top-[calc(22vh+112px)] left-3 sm:left-4 md:left-[24.5%] w-[2px] h-[2px] bg-black/50 dark:bg-white/[0.25] -translate-x-1/2 -translate-y-1/2 pointer-events-none z-10" />
-      <div className="absolute top-[calc(22vh+112px)] right-3 sm:right-4 md:right-[24.5%] w-[2px] h-[2px] bg-black/50 dark:bg-white/[0.25] translate-x-1/2 -translate-y-1/2 pointer-events-none z-10" />
 
       {/* Cell 1: Top Banner */}
       {bannerSlot ?? <TopBanner />}
