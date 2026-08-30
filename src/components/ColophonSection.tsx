c22e13f6edd37c018238a3a5cfe301d8ae41e4fc
VidZid2 <urbanninvidz2@gmail.com>
2026-08-30 15:57:06 +0800

    style(colophon): balance all footer icon sizes for consistent visual weight

--- src/components/ColophonSection.tsx
+++ src/components/ColophonSection.tsx
@@ -407,14 +407,14 @@ export function ColophonSection() {
                 : "text-zinc-400/40 dark:text-zinc-600/50 hover:text-red-500/80 dark:hover:text-red-400/80 active:text-red-500"
             )}
           >
-            <svg className="w-[18px] h-[18px] sm:w-[19px] sm:h-[19px]" viewBox="0 0 24 24" fill="currentColor">
+            <svg className="w-[23px] h-[23px] sm:w-[25px] sm:h-[25px]" viewBox="0 0 24 24" fill="currentColor">
               <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
             </svg>
           </motion.button>
 
           {/* Separator 1 */}
           <div
-            className="w-0 h-[15px] sm:h-[16px] border-r border-black/30 dark:border-white/[0.15] shrink-0 pointer-events-none"
+            className="w-0 h-[18px] sm:h-[20px] border-r border-black/30 dark:border-white/[0.15] shrink-0 pointer-events-none"
             style={dashedMaskVertical}
           />
 
@@ -426,14 +426,14 @@ export function ColophonSection() {
             aria-label="GitHub"
             className="hover:text-zinc-900 dark:hover:text-zinc-100 transition-colors flex items-center justify-center shrink-0 p-0.5"
           >
-            <svg className="w-[18px] h-[18px] sm:w-[19px] sm:h-[19px]" viewBox="0 0 24 24" fill="currentColor">
+            <svg className="w-[23px] h-[23px] sm:w-[25px] sm:h-[25px]" viewBox="0 0 24 24" fill="currentColor">
               <path fillRule="evenodd" clipRule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.53 1.032 1.53 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" />
             </svg>
           </a>
 
           {/* Separator 2 */}
           <div
-            className="w-0 h-[15px] sm:h-[16px] border-r border-black/30 dark:border-white/[0.15] shrink-0 pointer-events-none"
+            className="w-0 h-[18px] sm:h-[20px] border-r border-black/30 dark:border-white/[0.15] shrink-0 pointer-events-none"
             style={dashedMaskVertical}
           />
 
@@ -445,14 +445,14 @@ export function ColophonSection() {
             aria-label="LinkedIn"
             className="hover:text-zinc-900 dark:hover:text-zinc-100 transition-colors flex items-center justify-center shrink-0 p-0.5"
           >
-            <svg className="w-[18px] h-[18px] sm:w-[19px] sm:h-[19px]" viewBox="0 0 24 24" fill="currentColor">
+            <svg className="w-[23px] h-[23px] sm:w-[25px] sm:h-[25px]" viewBox="0 0 24 24" fill="currentColor">
               <path d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14m-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.28 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93h2.75M6.46 10.9v8.37H9.2V10.9H6.46M7.83 6.25c-.9 0-1.63.73-1.63 1.63s.73 1.63 1.63 1.63 1.63-.73 1.63-1.63-.73-1.63-1.63-1.63Z" />
             </svg>
           </a>
 
           {/* Separator 3 */}
           <div
-            className="w-0 h-[15px] sm:h-[16px] border-r border-black/30 dark:border-white/[0.15] shrink-0 pointer-events-none"
+            className="w-0 h-[18px] sm:h-[20px] border-r border-black/30 dark:border-white/[0.15] shrink-0 pointer-events-none"
             style={dashedMaskVertical}
           />
 
@@ -466,7 +466,7 @@ export function ColophonSection() {
             className="hover:text-zinc-900 dark:hover:text-zinc-100 transition-colors flex items-center justify-center shrink-0 p-0.5"
           >
             <svg
-              className="w-[25px] h-[25px] sm:w-[27px] sm:h-[27px]"
+              className="w-[28px] h-[28px] sm:w-[31px] sm:h-[31px]"
               viewBox="450 250 1500 1250"
               fill="currentColor"
             >
